661631c9e1374a1546431680dec61dcc630f927c
Loik Fekkai <loik@MacBook-Pro-de-Loik.local>
2020-07-05 12:18:14 +0200

    @reduxjs-toolkit : middleware use thunk

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import reducer from './reducer';
 import logger from './middleware/logger';
-import thunk from './middleware/thunk';
 
 export default function() {
     return configureStore({ 
         reducer,
-        middleware: [logger("console"), thunk]
+        middleware: [
+            ...getDefaultMiddleware(),
+            logger("console")
+        ]
      });
 };
