ed60bcbbc1dc212cf5bf8528cea6e3fdd147d2dc
Loik Fekkai <loik@MacBook-Pro-de-Loik.local>
2020-07-05 11:52:56 +0200

    @reduxjs-toolkit : middleware without param

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import reducer from './reducer';
+import logger from './middleware/logger';
 
-export default function () {
-    return configureStore({ reducer });
+export default function() {
+    return configureStore({ 
+        reducer,
+        middleware: [logger]
+     });
 };
